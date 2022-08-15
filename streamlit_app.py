f3c97601edea2fb5d951d84407fc667752f90a72
cuentaprueba95 <111072905+cuentaprueba95@users.noreply.github.com>
2022-08-15 13:52:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#Crea un bloque de código repetible (Función)
+def get_fruityvice_data(this_fruit_choice):
+  #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Toma el json y lo convierte a su formato normal
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 # New Section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -32,12 +40,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    #import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # Toma el json y lo convierte a su formato normal
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # Se envía a la pantalla como una tabla
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    # El parametro dataframe sirve para envíar a la pantalla nuestros datos en formato tabla
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 streamlit.stop()
